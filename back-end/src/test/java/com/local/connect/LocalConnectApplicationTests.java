09fd94a2d4ae14c8a3af09473958b302d79f7281
maenghansoo <aodtn929@g.shingu.ac.kr>
2026-04-08 15:28:24 +0900

    개발환경 구축

---
+++ back-end/src/test/java/com/local/connect/LocalConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
